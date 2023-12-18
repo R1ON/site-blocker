924314afb39254e7415132cf0d2fada8322af86f
Роман Максимов <r.maximov@banki.ru>
2023-12-18 20:35:21 +0300

    signUp + jwt + cookie

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PasswordService } from './password.service';
@@ -6,7 +7,11 @@ import { CookieService } from './cookie.service';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JwtModule.register({
+    global: true,
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '1d' },
+  })],
   controllers: [AuthController],
   providers: [AuthService, PasswordService, CookieService]
 })
--- server/src/auth/cookie.service.ts
+++ server/src/auth/cookie.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Response } from 'express';
+
+
+const ONE_DAY = 24 * 60 * 60 * 1000;
 
 @Injectable()
-export class CookieService {}
+export class CookieService {
+  static tokenKey = 'access-token';
+
+  setToken(res: Response, token: string) {
+    res.cookie(CookieService.tokenKey, token, { httpOnly: true, maxAge: ONE_DAY });
+  }
+
+  removeToken(res: Response) {
+    res.clearCookie(CookieService.tokenKey);
+  }
+}
