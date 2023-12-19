17136c33cff74dd0992533433305e249a4b35c02
Роман Максимов <r.maximov@banki.ru>
2023-12-19 22:45:00 +0300

    create account service

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { DbService } from '../db/db.service';
+import { AccountService } from '../account/account.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private dbService: DbService) {}
+  constructor(
+    private dbService: DbService,
+    private accountService: AccountService,
+  ) {}
 
   findByEmail(email: string) {
     return this.dbService.user.findFirst({ where: { email } });
   }
 
-  createUser(email: string, hash: string, salt: string) {
-    return this.dbService.user.create({ data: { email, hash, salt } });
+  async createUser(email: string, hash: string, salt: string) {
+    const user = await this.dbService.user.create({ data: { email, hash, salt } });
+
+    await this.accountService.createAccount(user.id);
+
+    return user;
   }
 }
